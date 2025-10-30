fb0b2eac8f3652961bf3ad1869227d554c11102e
vladek.lm <vladek.lm@mail.ru>
2025-10-30 12:49:12 +0300

    Init

---
+++ src/test/java/ru/mifiSoul/MultiBankApp/MultiBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mifiSoul.MultiBankApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiBankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
