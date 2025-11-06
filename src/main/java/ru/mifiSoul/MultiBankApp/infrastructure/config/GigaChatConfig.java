f63bef329d47a1e2086d6803bdf4401ec2326390
violet6bee <DeadViolet@yandex.ru>
2025-11-06 17:33:13 +0300

    create gigaChatAPI

---
+++ src/main/java/ru/mifiSoul/MultiBankApp/infrastructure/config/GigaChatConfig.java
@@ -0,0 +1,28 @@
+package ru.mifiSoul.MultiBankApp.infrastructure.config;
+
+import chat.giga.client.GigaChatClient;
+import chat.giga.client.auth.AuthClient;
+import chat.giga.client.auth.AuthClientBuilder;
+import chat.giga.model.Scope;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GigaChatConfig {
+
+    @Value("${gigachat.auth-key}")
+    private String authKey;
+
+    @Bean
+    public GigaChatClient gigaChatClient() {
+        return GigaChatClient.builder()
+                .authClient(AuthClient.builder()
+                        .withOAuth(AuthClientBuilder.OAuthBuilder.builder()
+                                .scope(Scope.GIGACHAT_API_PERS)
+                                .authKey(authKey)
+                                .build())
+                        .build())
+                .build();
+    }
+}
